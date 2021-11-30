a451cf2c908c7c30a4da45c50abd17b4797c1413
Sean Orosz <seanorosz@yahoo.com>
2021-11-30 14:54:55 -0500

    V1.01

--- src/index.js
+++ src/index.js
@@ -1,1 +1,29 @@
-Write code here...
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+
+class App extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      date: new Date(),
+    }
+  }
+
+  render() {
+    const day = days[this.state.date.getDay()];
+    return(
+      <div id='wrapper'>
+        <h1>Hello World!</h1>
+        <p>It's {day}</p>
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+);
