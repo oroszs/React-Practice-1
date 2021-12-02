84f7c4dd22b1c099b0b32507316bf0acf91726c7
Sean Orosz <seanorosz@yahoo.com>
2021-12-01 22:59:39 -0500

    V1.1

--- src/index.js
+++ src/index.js
@@ -13,11 +13,17 @@ class App extends React.Component {
 
   createDeck(){
     const faces = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'Jack', 'Queen', 'King', 'Ace'];
-    const suits = ['Spades', 'Clubs', 'Diamonds', 'Hearts'];
+/*
+♠	Black Spade	&#9824;
+♥	Black Heart	&#9829;
+♣	Black Club	&#9827;
+♦	Black Diamond	&#9830;
+*/
+    const suits = ['\u2660', '\u2663', '\u2665', '\u2666'];
     const fullDeck = [];
     suits.forEach(suit => {
       faces.forEach(face => {
-        fullDeck.push(`${face} of ${suit}, `);
+        fullDeck.push(`${face} ${suit}`);
       });
     });
     return fullDeck;
@@ -33,14 +39,23 @@ class App extends React.Component {
 }
 
 class Game extends React.Component {
-  constructor(props){
-    super(props);
+
+  createCards(){
+    let cards = [];
+    this.props.deck.forEach(card => {
+      cards.push(
+        <div className='card' key={card}>
+          <div className='cardText'>{card}</div>
+        </div>
+      );
+    });
+    return cards;
   }
 
   render(){
     return(
       <div id='cardDisplay'>
-        {this.props.deck}
+        {this.createCards()}
       </div>
     );
   }
