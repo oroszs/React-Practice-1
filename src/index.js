b03e3ab859984fd649c7877ddddb4beaeb88e5da
Sean Orosz <seanorosz@yahoo.com>
2021-12-01 23:13:18 -0500

    V1.2

--- src/index.js
+++ src/index.js
@@ -13,16 +13,12 @@ class App extends React.Component {
 
   createDeck(){
     const faces = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'Jack', 'Queen', 'King', 'Ace'];
-/*
-♠	Black Spade	&#9824;
-♥	Black Heart	&#9829;
-♣	Black Club	&#9827;
-♦	Black Diamond	&#9830;
-*/
     const suits = ['\u2660', '\u2663', '\u2665', '\u2666'];
     const fullDeck = [];
     suits.forEach(suit => {
       faces.forEach(face => {
+        (suit === '\u2665' || suit === '\u2666') ?
+        fullDeck.push(<span>{face} <span className='redSuit'>{suit}</span></span>) :
         fullDeck.push(`${face} ${suit}`);
       });
     });
