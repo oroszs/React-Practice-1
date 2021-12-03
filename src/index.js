ce803fa9ab9f640f9d29191665b68bf55fdd257e
Sean Orosz <seanorosz@yahoo.com>
2021-12-03 15:06:49 -0500

    V1.6

--- src/index.js
+++ src/index.js
@@ -20,7 +20,7 @@ class Game extends React.Component {
     this.state = {
       currentDeck: this.createDeck(),
     }
-    this.dealHand = this.dealHand.bind(this);
+    this.dealCards = this.dealCards.bind(this);
   }
 
   createDeck(){
@@ -35,7 +35,7 @@ class Game extends React.Component {
     return fullDeck;
   }
 
-  dealHand(handSize){
+  dealCards(handSize){
     let hand = [];
     const deck = this.state.currentDeck;
     for(let x = 0; x < handSize; x++){
@@ -51,27 +51,24 @@ class Game extends React.Component {
   }
 
   componentDidMount(){
-    const deck = this.state.currentDeck;
-    const hand1 = this.createCards(this.dealHand(5));
-    const hand2 = this.createCards(this.dealHand(5));
-    const hand3 = this.createCards(this.dealHand(5));
-    const thisDeck = this.createCards(deck);
+    const board = this.createCards(this.dealCards(5), false);
+    const p1 = this.createCards(this.dealCards(2), false);
+    const deck = this.createCards(this.state.currentDeck, true);
     this.setState({
-      hand1: hand1,
-      hand2: hand2,
-      hand3: hand3,
-      dispDeck: thisDeck,
+      board: board,
+      p1Hand: p1,
+      deck: deck,
     });
   }
 
-  createCards(cards){
+  createCards(cards, deck){
     let divCards = [];
     for(let x = 0; x < cards.length; x++){
       divCards.push(
-        <div className='card' key={cards[x]} >
+        <div className={deck ? 'deckCard' : 'card'} key={cards[x]} >
           {cards[x].includes('\u2665') || cards[x].includes('\u2666') ?
-          <div className='cardText'>{cards[x].split(' ')[0]} <span style={{color: 'red'}}>{cards[x].split(' ')[1]}</span></div> :
-          <div className='cardText'>{cards[x]}</div>}
+          <div className={deck ? 'deckCardText' : 'cardText'}>{cards[x].split(' ')[0]} <span style={{color: 'red'}}>{cards[x].split(' ')[1]}</span></div> :
+          <div className={deck ? 'deckCardText' : 'cardText'}>{cards[x]}</div>}
         </div>
       );
     }
@@ -79,15 +76,15 @@ class Game extends React.Component {
   }
 
   render(){
+    const board = this.state.board;
+    const p1Hand = this.state.p1Hand;
+    const deck = this.state.deck;
     return(
       <div>
         <div id='cardDisplay'>
-          {this.state.dispDeck}
-          <div id='playerArea'>
-            <div>{this.state.hand1}</div>
-            <div>{this.state.hand2}</div>
-            <div>{this.state.hand3}</div>
-          </div>
+          <div id='board' className='cardHolder'>{board}</div>
+          <div id='deckDisplay' className='cardHolder'>{deck}</div>
+          <div id='playerArea' className='cardHolder'>{p1Hand}</div>
         </div>
       </div>
     );
